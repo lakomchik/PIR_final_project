d3a2f7b4bea6f43fb6115649a9849224db145a8c
HelloWorld21951 <niknaglov99@gmail.com>
2023-03-20 12:41:01 +0300

    Refactor data_reader

--- tools/data_reader.py
+++ tools/data_reader.py
@@ -1,33 +1,49 @@
+import dataclasses
 import linecache
-
-dataset_path = "/home/lakomchik/sata/slam_dataset/rgbd_dataset_freiburg1_xyz/"
-
-accelerometer_name = "accelerometer.txt"
-depth_name = "depth.txt"
-rgb_name = "rgb.txt"
-gt_name = "groundtruth.txt"
-
 import cv2
+import numpy as np
 
-initial_offset = 4
+
+@dataclasses.dataclass
+class Observation:
+    timestamp: float
+    image: np.ndarray
+    depth: np.ndarray
 
 
-def get_observations(idx):
-    """Provides timestamp, rgb_image and depth image by step
+def get_filepath(folder: str, filename: str) -> str:
+    return f"{folder}/{filename}"
+
+
+def get_observation(
+    idx: int,
+    dataset_folder: str = "datasets/rgbd_dataset_freiburg1_xyz",
+    image_filename: str = "rgb.txt",
+    depth_filename: str = "depth.txt",
+    starting_offset: int = 4,
+) -> Observation:
+    """
+    Gets an observation by step number
 
     Args:
-        idx (int): step number
+        idx (int): Step number
+        dataset_folder (str, optional): Path to dataset folder. Defaults to "datasets/rgbd_dataset_freiburg1_xyz"
+        image_filename (str, optional): Name of file with image RGB data. Defaults to "depth.txt"
+        depth_filename (str, optional): Name of file with image depth data. Defaults to "rgb.txt"
+        starting_offset (int, optional): Index of initial observation. Defaults to 4
 
     Returns:
-        float, cv_img, cv_img: timestamp, rgb_img, depth_img
+        Observation: Observation at the given step
     """
     timestamp, img_loc = linecache.getline(
-        dataset_path + rgb_name, initial_offset + idx
+        filename=get_filepath(dataset_folder, image_filename),
+        lineno=starting_offset + idx,
     ).split()
     _, depth_loc = linecache.getline(
-        dataset_path + depth_name, initial_offset + idx
+        filename=get_filepath(dataset_folder, depth_filename),
+        lineno=starting_offset + idx,
     ).split()
-    img = cv2.imread(dataset_path + img_loc)
-    depth = cv2.imread(dataset_path + depth_loc)
+    image = cv2.imread(dataset_folder + img_loc)
+    depth = cv2.imread(dataset_folder + depth_loc)
     timestamp = float(timestamp)
-    return timestamp, img, depth
+    return Observation(timestamp, image, depth)
