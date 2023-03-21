3b46ea67977c67333db5ff0051303e87467418e1
Ivan Apanasevich <ivanapanasevich@mail.ru>
2023-03-21 19:08:53 +0300

    Added graph sla

--- graph_slam.py
+++ graph_slam.py
@@ -43,7 +43,6 @@ class GraphSlam:
             if depth[y_px, x_px] == 0:
                 continue
             node = self.graph.add_node_landmark_3d(np.zeros(3))
-            # print(ds)
             self.detected_features[tuple(ds)] = node
             coords = point2xyz(np.array([x_px, y_px]), depth).to_np_array()
             W = np.identity(3)
@@ -60,43 +59,72 @@ class GraphSlam:
             coords = point2xyz(np.array([x_px, y_px]), depth).to_np_array()
             W = np.identity(3)
             self.graph.add_factor_1pose_1landmark_3d(
-                coords, self.poses_id[-1], self.detected_features[ds], W
+                coords, self.poses_id[-1], self.detected_features[tuple(ds)], W
             )
 
     def step(self, img, depth):
-        # self.graph.add
+        node = self.graph.add_node_pose_3d(mrob.geometry.SE3(np.zeros([6])))
+        self.poses_id.append(node)
+
+        self.graph.add_factor_2poses_3d(
+            mrob.geometry.SE3(),
+            self.poses_id[-2],
+            self.poses_id[-1],
+            1e6 * np.identity(6),
+        )
+        self.process_image_and_depth(img, depth)
         pass
 
     def process_image_and_depth(self, img, depth):
         kp, ds = self.get_img_features(img)
+
         features_list = list(self.detected_features.keys())
-        matches = self.bf.knnMatch(features_list, ds)
+        features_arr = np.asarray(features_list)
+        matches = self.bf.knnMatch(ds, features_arr, k=2)
         # Apply ratio test
         good = []
         for m, n in matches:
-            if m.distance < 0.3 * n.distance:
+            if m.distance < 0.9 * n.distance:
                 good.append([m])
 
         similarity_mask = np.zeros_like(kp, dtype=bool)
         matched_idxs = []
         updated_features = []
         for match in good:
-            similarity_mask[match[0].trainIdx] = True
-            updated_features.append(features_list[match[0].queryIdx])
+            similarity_mask[match[0].queryIdx] = True
+            updated_features.append(features_list[match[0].trainIdx])
+        _, counts = np.unique(similarity_mask, return_counts=True)
         kp_arr = np.asarray(kp)
         ds_arr = np.asarray(ds)
         updated_kp = kp_arr[similarity_mask]
         new_kp = kp_arr[np.logical_not(similarity_mask)]
         new_ds = ds_arr[np.logical_not(similarity_mask)]
         self.add_3d_landmarks(new_kp, new_ds, depth)
-        self.update_3d_landmarks(updated_kp, updated_features)
+        self.update_3d_landmarks(updated_kp, updated_features, depth)
 
 
-observation = get_observation(1)
-print(observation.depth[0, 0])
-cv2.imshow("dad", observation.depth)
-cv2.waitKey(0)
-vis_odom = GraphSlam(observation.image, observation.depth)
+observation = get_observation(0)
+# print(observation.image[0, 0])
+# cv2.imshow("dad", observation.depth)
+# cv2.waitKey(0)
+graph_slam = GraphSlam(observation.image, observation.depth)
+print("Amount of descriptions in dictionary is", len(graph_slam.detected_features))
 cv2.imshow("dad", observation.image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+chi2 = []
+for i in range(1, 100):
+    observation = get_observation(i)
+    # print(len(graph_slam.detected_features))
+    graph_slam.step(observation.image, observation.depth)
+    print("Amount of descriptions in dictionary is", len(graph_slam.detected_features))
+    graph_slam.graph.solve(mrob.LM)
+    chi2.append(graph_slam.graph.chi2())
+
+
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.use("TkAgg")
+plt.plot(np.arange(len(chi2)), chi2)
+plt.show()
