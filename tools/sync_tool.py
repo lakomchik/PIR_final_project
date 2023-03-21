b113d1011936eeec638c4d4f1cc28598adc2a41f
Ant3461 <sergeydavidenko3461@gmail.com>
2023-03-22 00:29:08 +0300

    sync tool done

--- tools/sync_tool.py
+++ tools/sync_tool.py
@@ -5,75 +5,100 @@ import glob
 import pandas as pd
 
 
+class Sync():
+    """
+    # example of using
+    xyz_ds=Sync()
+    xyz_ds.init(path_xyz=r"datasets/rgbd_dataset_freiburg1_xyz_clear_test")
+    xyz_ds.sort()
+    xyz_ds.write_txt_sync()
+    xyz_ds.write_txt_ground_truth()
+    xyz_ds.get_ground_truth_sync(50)
+    """
+    def init(self,path_xyz):
+        """
+        reading data from .txt files
+        args:
+        path_xyz- path of directory, where .txt files is located
+        """
+        ###
+        self.path_xyz=path_xyz
 
+        files=glob.glob(path_xyz+'\*')
+        for i in range(0,len(files)):
+            files[i]=files[i][47:]
 
-def sync_file_gen(path_,name='test_sync'):
-    '''
-    This function generate 
-    '''
-    # path_= r'datasets/rgbd_dataset_freiburg1_xyz_clear_test'
+        print(files)
+        ###
+        self.depth_data = pd.read_csv(path_xyz+"/"+'depth.txt', delim_whitespace=True,header=None,skiprows=3)
+        self.depth_data.columns = ["timestamp", "path"]
+        ###
+        self.rgb_data = pd.read_csv(path_xyz+"/"+'rgb.txt', delim_whitespace=True,header=None,skiprows=3)
+        self.rgb_data.columns = ["timestamp", "path"]
+        ###
+        self.groundtruth_data=pd.read_csv(path_xyz+"/"+'groundtruth.txt', delim_whitespace=True,header=None,skiprows=3)
+        self.groundtruth_data.columns = ["timestamp", "tx", "ty", "tz", "qx", "qy", "qz", "qw"]
+        ###
+        
+    def sort(self):
+        """
+        synchronize timestemps between groundtruth and depth 
+        """
+        self.res=np.zeros(len(self.depth_data.timestamp))
 
-    files=glob.glob(path_+'\*')
-    for i in range(0,len(files)):
-        files[i]=files[i][47:]
-
-    # print(files)
-
-
-    for i in files[2],files[6]:
-        globals()['%s_data'%i[:-4]] = pd.read_csv(path_+"/"+i, delim_whitespace=True, error_bad_lines=False,header=None,skiprows=3)
-        globals()['%s_data'%i[:-4]].columns = ["timestamp", "path"]
-
-    groundtruth_data=pd.read_csv(path_+"/"+files[3], delim_whitespace=True, error_bad_lines=False,header=None,skiprows=3)
-    groundtruth_data.columns = ["timestamp", "tx", "ty", "tz", "qx", "qy", "qz", "qw"]
-
-
-    res=np.zeros(len(depth_data.timestamp))
-
-    for j in range(0,len(depth_data.timestamp) - 1):
-        error=1e13
-        for i in range(0,len(groundtruth_data.timestamp) - 1):
-            # print("error= ",np.abs(groundtruth_data.timestamp[i] - depth_data.timestamp[j]))
-            if np.abs(groundtruth_data.timestamp[i] - depth_data.timestamp[j])<error:
-                error=np.abs(groundtruth_data.timestamp[i] - depth_data.timestamp[j])
-                # print('j=',j,' i=',i," error=",error,'\n')
-                res[j]=i        
-
-
-    with open(path_+"/"+name+'.txt', 'w') as f:
-        f.write("timestamp depth_path rgb_path \n")
-        for i in range(0,len(res)-1) :
-            f.write(str(groundtruth_data.timestamp[int(res[i])])+' '+str(depth_data.path[i])+' '+str(rgb_data.path[i]))
-            f.write('\n')
+        for j in range(0,len(self.depth_data.timestamp) - 1):
+            error=1e13
+            for i in range(0,len(self.groundtruth_data.timestamp) - 1):
+                # print("error= ",np.abs(groundtruth_data.timestamp[i] - depth_data.timestamp[j]))
+                if np.abs(self.groundtruth_data.timestamp[i] - self.depth_data.timestamp[j])<error:
+                    error=np.abs(self.groundtruth_data.timestamp[i] - self.depth_data.timestamp[j])
+                    # print('j=',j,' i=',i," error=",error,'\n')
+                    self.res[j]=i          
     
-    #ground_true gen
-    
+    def write_txt_sync(self):
+        """
+        create .txt files, which has associated data such as timestemp,path to depth image and path to rgb image
+        """
+        with open(self.path_xyz+"/"+'test_sync.txt', 'w') as f:
+            f.write("timestamp depth_path rgb_path \n")
+            for i in range(0,len(self.res)-1) :
+                f.write(str(self.groundtruth_data.timestamp[int(self.res[i])])+' '+str(self.depth_data.path[i])+' '+str(self.rgb_data.path[i]))
+                f.write('\n')
 
-    with open(path_+"/"+"groundtruth_sync"+'.txt', 'w') as f:
-        f.write("timestamp tx ty tz qx qy qz qw \n")
-        for i in range(0,len(res)-1) :
+    def write_txt_ground_truth(self):
+        """
+        create .txt file with synhronize timestemp
+        """
+        with open(self.path_xyz+"/"+'ground_truth_sync.txt', 'w') as f:
+            f.write("timestamp tx ty tz qx qy qz qw \n")
+            for i in range(0,len(self.res)-1) :
+                
+                timestamp=(self.groundtruth_data.timestamp[int(self.res[i])])
+                tx=(self.groundtruth_data.tx[int(self.res[i])])
+                ty=(self.groundtruth_data.ty[int(self.res[i])])
+                tz=(self.groundtruth_data.tz[int(self.res[i])])
+                qx=(self.groundtruth_data.qx[int(self.res[i])])
+                qy=(self.groundtruth_data.qy[int(self.res[i])])
+                qz=(self.groundtruth_data.qz[int(self.res[i])])
+                qw=(self.groundtruth_data.qw[int(self.res[i])])
+                pose=np.array([timestamp,tx,ty,tz,qx,qy,qz,qw])
 
-            tx=(groundtruth_data.tx[int(res[i])])
-            ty=(groundtruth_data.ty[int(res[i])])
-            tz=(groundtruth_data.tz[int(res[i])])
-            qx=(groundtruth_data.qx[int(res[i])])
-            qy=(groundtruth_data.qy[int(res[i])])
-            qz=(groundtruth_data.qz[int(res[i])])
-            qw=(groundtruth_data.qw[int(res[i])])
-            pose=np.array([tx,ty,tz,qx,qy,qz,qw])
+                f.write(str(pose[0])+' '+str(pose[1])+' '+str(pose[2])+' '+str(pose[3])+' '+str(pose[4])+' '+str(pose[5])+' '+str(pose[6])+' '+str(pose[7]))
+                f.write('\n')
+        
+    def get_ground_truth_sync (self,i:int):
+        """
+        get pose on current timestemp
 
-            f.write(pose[0]+' '+pose[1]+' '+pose[2]+' '+pose[3]+' '+pose[4]+' '+pose[5]+' '+pose[6])
-            f.write('\n')
-
-def get_pose(i):
-    # with open(path_+"/"+"groundtruth_sync"+'.txt', 'w') as f:
-    tx=(groundtruth_data.tx[int(res[i])])
-    ty=(groundtruth_data.ty[int(res[i])])
-    tz=(groundtruth_data.tz[int(res[i])])
-    qx=(groundtruth_data.qx[int(res[i])])
-    qy=(groundtruth_data.qy[int(res[i])])
-    qz=(groundtruth_data.qz[int(res[i])])
-    qw=(groundtruth_data.qw[int(res[i])])
-    pose=np.array([tx,ty,tz,qx,qy,qz,qw])
-    return pose
-
+        Args:
+            i (int): the number of timestemp
+        """
+        tx=(self.groundtruth_data.tx[int(self.res[i])])
+        ty=(self.groundtruth_data.ty[int(self.res[i])])
+        tz=(self.groundtruth_data.tz[int(self.res[i])])
+        qx=(self.groundtruth_data.qx[int(self.res[i])])
+        qy=(self.groundtruth_data.qy[int(self.res[i])])
+        qz=(self.groundtruth_data.qz[int(self.res[i])])
+        qw=(self.groundtruth_data.qw[int(self.res[i])])
+        pose=np.array([tx,ty,tz,qx,qy,qz,qw])
+        return(pose)
